5bc6aa1f723258cb73f54d5cc2f838b0a49930b9
Unknown <franek.stark@haw-hamburg.de>
2018-05-28 22:44:58 +0200

    init Project

---
+++ src/application/service/BPMDetectionService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package application.service;
+
+/**
+ * Detects the BPM by time different Triggers.
+ * @author Franek Stark
+ *
+ */
+class BPMDetectionService {
+	private double _bpm;
+	private long _lastTrigger; 
+	
+	public double trigger(){
+		
+		return 0.0;
+	}
+}
